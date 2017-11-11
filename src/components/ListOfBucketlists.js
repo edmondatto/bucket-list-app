bb6566576704744cbc1af1b5cc3c3bbca5027b44
Edmond Atto <edmond091@gmail.com>
2017-11-11 10:39:17 +0300

    Create container component to render bucket lists

---
+++ src/components/ListOfBucketlists.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import BucketList from "./BucketLists/BucketList";
+
+const ListOfBucketlists = props =>
+  <div className="row">
+    <BucketList />
+  </div>;
+
+export default ListOfBucketlists;
