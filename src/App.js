828f48f7e456d844d4b08d22fa7788361448f2bb
Edmond Atto <edmond091@gmail.com>
2017-11-11 10:42:08 +0300

    Add constituent components to main app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import SearchArea from './components/SearchArea';
+import ListOfBucketlists from "./components/ListOfBucketlists";
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
-    return (
 
+
+    return (
+      <div>
+        <Header/>
+        <div className="container-fluid" id="main-container">
+          <SearchArea />
+          <ListOfBucketlists/>
+        </div>
+        <Footer/>
+      </div>
     );
   }
 }
