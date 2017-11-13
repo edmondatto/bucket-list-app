74e38f7b22b63f36c975110b045abab6495c5c04
Edmond Atto <edmond091@gmail.com>
2017-11-13 20:27:13 +0300

    Add actiontypes for bucketlists

---
+++ src/actiontypes/bucketlist.js
@@ -0,0 +1,5 @@
+export const BUCKETLISTS_FETCH = 'bucketlists/BUCKETLISTS_FETCH';
+export const BUCKETLIST_CREATE = 'bucketlists/BUCKETLIST_CREATE';
+export const BUCKETLIST_EDIT = 'bucketlists/BUCKETLIST_EDIT';
+export const BUCKETLIST_DELETE = 'bucketlists/BUCKETLIST_DELETE';
+export const BUCKETLISTS_SEARCH = 'bucketlists/BUCKETLISTS_FETCH';
