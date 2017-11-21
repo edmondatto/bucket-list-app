4c92b9309efbbb8a30a630787788765751c74122
Edmond Atto <edmond091@gmail.com>
2017-11-21 21:44:19 +0300

    Update API base URL

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://johnzbucketlist-staging.herokuapp.com/api/v1/';
+// export const BASE_URL = 'https://johnzbucketlist-staging.herokuapp.com/api/v1/';
+export const BASE_URL = 'http://kbucket-api.herokuapp.com/v1/';
+export const BUCKETLISTS_URL = BASE_URL + 'bucketlists/';
