39e8e191e9986d37061da1b8cc8a8070fdd5d360
Edmond Atto <edmond091@gmail.com>
2017-11-21 21:49:18 +0300

    Add fetch bucketlists smart and dumb actions

--- src/actions/bucketlists.js
+++ src/actions/bucketlists.js
@@ -1,7 +1,31 @@
 import * as BucketlistActionTypes from '../actiontypes/bucketlist';
+import axios from 'axios';
+import * as CONSTANTS from "../utilities/constants";
 
-export const fetchBucketLists = () => {
+
+export const fetchBucketLists = (data) => {
   return {
-    type: BucketlistActionTypes.BUCKETLISTS_FETCH
+    type: BucketlistActionTypes.BUCKETLISTS_FETCH_SUCCESS,
+    data
+  }
+};
+
+export const fetchBucketlistsFromApi = () => {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const config = {
+    method: 'get',
+    url: proxyurl + CONSTANTS.BUCKETLISTS_URL,
+    headers: {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTEzNTkzOTYsImlhdCI6MTUxMTI3Mjk3Niwic3ViIjozfQ.PKEnix_Ek79b0q1Q4Yiy-BO4jzMyW5EOCutJnQ9vp2w'}
+  };
+  return dispatch => {
+    return axios(config)
+      .then(response => response.data)
+      .then(data => {
+        console.log(data);
+        dispatch(fetchBucketLists(data))
+      })
+      .catch(error => {
+        console.log('it errored!', config);
+      })
   }
 };
